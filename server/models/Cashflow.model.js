cc12cc9f45057cbf5a41257a584d41dbc21c2ae9
varaa <avarap@gmail.com>
2022-11-30 23:04:49 +0100

    ?

--- server/models/Cashflow.model.js
+++ server/models/Cashflow.model.js
@@ -1,25 +1,28 @@
- const { Schema, model, default: mongoose } = require("mongoose");
- const findOrCreate = require("mongoose-findorcreate");
- const cashFlowSchema = new Schema(
-   {
-     valueDate: Date,
-     description: String,
-     totalAmount: Number,
-     overall: [
-       {
-         percentage: Number,
-         paid: Boolean,
-         user: { type: Schema.Types.ObjectId, ref: "User" },
-       },
-     ],
-     picture: String,
-     owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-   },
-   {
-     // this second object adds: `createdAt`, `updatedAt`
-     timestamps: true,
-   }
- );
- cashFlowSchema.plugin(findOrCreate);
- const CashFlow = model("CashFlow", cashFlowSchema);
- module.exports = CashFlow;
+const { Schema, model, default: mongoose } = require("mongoose");
+const findOrCreate = require("mongoose-findorcreate");
+
+const cashFlowSchema = new Schema(
+  {
+    valueDate: Date,
+    description: String,
+    totalAmount: Number,
+    overall: [
+      {
+        percentage: Number,
+        paid: Boolean,
+        user: { type: Schema.Types.ObjectId, ref: "User" },
+      },
+    ],
+    picture: String,
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  {
+    // this second object adds: `createdAt`, `updatedAt`
+    timestamps: true,
+  }
+);
+
+cashFlowSchema.plugin(findOrCreate);
+const CashFlow = model("CashFlow", cashFlowSchema);
+
+module.exports = CashFlow;
