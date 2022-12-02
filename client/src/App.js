321569de6dfe4ce9e680b2d9b3101145c0affd8f
herrFernandez <insideberlin17@gmail.com>
2022-12-02 13:28:59 +0100

    Profile avatar and links added in the navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home"
 import SignUp from "./pages/SignUp";
 import Login2 from "./pages/Login2";
 import Profile from "./pages/Profile";
+import Dashboard from "./pages/Dashboard";
 import Navbar from "./components/Navbar"
 import { Routes, Route} from "react-router-dom";
 import { UserContext } from "./contexts/UserContextProvider"
@@ -51,7 +52,7 @@ function App() {
 
 	return (
 		<div className="container">
-			<Navbar />
+			
 			<Routes>
 				<Route
 					exact
@@ -66,7 +67,12 @@ function App() {
 				<Route
 					exact
 					path="/profile"
-					element={<Profile  /> }
+					element={[<Navbar />, <Profile  />] }
+				/>
+				<Route
+					exact
+					path="/dashboard"
+					element={<Dashboard /> }
 				/>
 				<Route
 					exact
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,53 +1,58 @@
 import React from 'react'
-import axios from "axios"
-import { useNavigate } from "react-router-dom"
 import { useContext } from 'react';
 import { UserContext } from '../contexts/UserContextProvider';
+
+import Avatar from '@mui/material/Avatar';
+
 
 function Profile() {
 
 	const { userObject } = useContext(UserContext);
 
-	const navigate = useNavigate();
 
-	function logout() {
-		axios({ method: "POST", url: `${process.env.REACT_APP_API_URL}/auth/logout`, withCredentials: true })
-			.then((response) => {
-				console.log("response status", response)
-				if (response.status === 200) {
-					navigate("/login");
-					return null;
-				}
-			})
+	// function logout() {
+	// 	axios({ method: "POST", url: `${process.env.REACT_APP_API_URL}/auth/logout`, withCredentials: true })
+	// 		.then((response) => {
+	// 			console.log("response status", response)
+	// 			if (response.status === 200) {
+	// 				navigate("/login");
+	// 				return null;
+	// 			}
+	// 		})
 
-	}
+	// }
+
+	
 
 	return (
 		<div>
 			{
 				userObject ? (
+					<>
 					<h1>Welcome back {userObject.googleId}</h1>
+					<h2>{userObject.displayName}</h2>
+					</>
 				) : (
 					<h1>Welcome To MY Website</h1>
 				)
 			}
 
-			<button onClick={logout}>Log Out</button>
-		</div>
-		// <div >
-		// 	<h1>Profile page: {userObject.googleId}</h1>
-		// 	{/* {{userObject} ? (<p>{userObject.googleId}</p>) : ("Hasta la vista")} */}
+			{	userObject ? (
+				<>
+					<h1>{userObject.displayName}</h1>
+					<img src={`${userObject.picture}`} width="32px" alt=''/>
+					<Avatar sx={{ width: 32, height: 32 }} src={`${userObject.picture}`}></Avatar>
+				</>
+				) : (
+					<Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
+				) 
+			}
+        
 
-		// 	<img
-		// 				// src={user.picture}
-		// 				alt="profile"
-		// 				className="picture-home"
-		// 			/>
-		// 	<button  onClick={logout}>
-		// 				Log Out
-		// 			</button>
-		// </div>
-	);
-}
+			{/* <button onClick={logout}>Log Out</button> */}
+		</div>
+	)
+		}
+
 
 export default Profile;
