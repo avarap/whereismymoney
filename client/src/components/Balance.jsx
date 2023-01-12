4f0603f07762f4f497773a9f40d2965e707f0401
varaa <avarap@gmail.com>
2023-01-12 23:23:32 +0100

    Delete done

--- client/src/components/Balance.jsx
+++ client/src/components/Balance.jsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
-import Link from '@mui/material/Link';
+// import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 import Title from './Title';
 
-function preventDefault(event) {
-  event.preventDefault();
-}
+// function preventDefault(event) {
+//   event.preventDefault();
+// }
 
 function getMonthName(monthNumber) {
   const date = new Date();
-  date.setMonth(monthNumber - 1);
+  date.setMonth(monthNumber );
 
   return date.toLocaleString("en-US", { month: "long" });
 }
@@ -20,24 +20,11 @@ export default function Deposits({data, year, month}) {
   return (
     <React.Fragment>
       <Title>Recent Expenses</Title>
-      <Typography
-        component="p"
-        variant="h4">
-        €{currentAmount}
-      </Typography>
-      <Typography
-        color="text.secondary"
-        sx={{ flex: 1 }}>
-        on {getMonthName(month)}, {year}
-      </Typography>
-      <div>
-        <Link
-          color="text.secondary"
-          href="#"
-          onClick={preventDefault}>
-          View balance
-        </Link>
-      </div>
+      <Typography component="p" variant="h4">{currentAmount} €</Typography>
+      <Typography color="text.secondary" sx={{ flex: 1 }}>{getMonthName(month)}, {year}</Typography>
+      {/* <div>
+        <Link color="text.secondary" href="#" onClick={preventDefault}>View balance</Link>
+      </div> */}
     </React.Fragment>
   );
 }
