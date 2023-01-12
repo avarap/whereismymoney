0911d1550fceb74e928bb1abb156d6104f7b05c3
varaa <avarap@gmail.com>
2023-01-12 19:53:42 +0100

    Mongo_config

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 
 const mongoose = require("mongoose");
-
 const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/server";
+module.exports = { MONGO_URI };
 
 mongoose
   .connect(MONGO_URI)
