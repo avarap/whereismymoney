29e98f9f9836434be29d8a78aa1ba7c512f974d4
varaa <avarap@gmail.com>
2024-11-17 22:38:17 +0100

    Update Packages

--- server/db/index.js
+++ server/db/index.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/server";
 module.exports = { MONGO_URI };
 
+mongoose.set('strictQuery', false);
+
 mongoose
   .connect(MONGO_URI)
   .then((x) => {
